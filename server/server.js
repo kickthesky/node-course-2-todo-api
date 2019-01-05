82e410d1adaf131137478817008c529254878fe7
webappsecure <webappsecure@gmail.com>
2019-01-05 12:39:27 -0600

    Adding /todos/:id delete route

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.post('/todos', (req, res) => {
 
     todo.save().then((doc) => {
         res.send(doc);
-    }, (e) => {
+    }).catch((e) => {
         res.status(400).send(e);    
     });
 });
@@ -27,7 +27,7 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
-    }, (e) => {
+    }).catch((e) => {
         res.status(400).send(e);
     });
 });
@@ -42,7 +42,22 @@ app.get('/todos/:id', (req, res) => {
             return res.status(404).send();
         }
         res.send({todo});
-    }, (e) => {
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndDelete(req.params.id).then((todo) => { 
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
         res.status(400).send(e);
     });
 });
